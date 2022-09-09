418eed0cf1a4ca6f74def49a40099ee07806bc9d
jafegideon04 <arowologideon45@gmail.com>
2022-09-09 16:58:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-  
- /** 
-   * main - Prints combination of numbers 
-   * 
-   * Return: Always (Success) 
-   */ 
- int main(void) 
- { 
-         int c, i; 
-  
-         for (c = '0'; c <= '9'; c++) 
-         { 
-                 for (i = '0'; i <= '9'; i++) 
-                 { 
-                         if (c < i) 
-                         { 
-                                 putchar(c); 
-                                 putchar(i); 
-  
-                                 if (c != '8' || (c == '8' && i != '9')) 
-                                 { 
-                                         putchar(','); 
-                                         putchar(' '); 
-                                 } 
-                         } 
-                 } 
-         } 
-  
-         putchar('\n'); 
-  
-         return (0);
- }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b;
+
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+if (a == b || b <= a)
+{
+}
+else
+{
+putchar(a);
+putchar(b);
+if (a == 56 && b == 57)
+{
+}
+else
+{
+putchar(44);
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
