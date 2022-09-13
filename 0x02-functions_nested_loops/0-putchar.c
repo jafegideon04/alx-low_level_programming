c452ec9d0d456dcc961aa12cb1816fed42fce236
jafegideon04 <arowologideon45@gmail.com>
2022-09-13 04:02:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main() 
+int main(void)
 {
 	printf("_putchar\n");
-	return 0;
+	return (0);
 }
 
