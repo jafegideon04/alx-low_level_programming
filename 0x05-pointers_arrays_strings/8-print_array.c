6e4d65d501da2f3af9eb1bb4e06633696b7829c3
jafegideon04 <arowologideon45@gmail.com>
2022-09-19 15:35:54 -0700

    TASK ON ARRAYS, POINTERS AND STRINGS DONE

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,29 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_array - prints n elements of an array of integers
- *@a: A pointer to an int that will be updated/ changed
- *@n: return value n
- *Return: void which ,means our answer is correct
+ *print_array - prints n elements of an array of integers.
+ *@a: pointer to array.
+ *@n: number of array elements.
+ *
+ *Return: void.
  */
-
 void print_array(int *a, int n)
-
 {
-int i;
+	int i;
 
-i = 0;
-while (i < n)
-{
-printf("%d", a[i]);
-
-if (i < n - 1)
-{
-printf(", ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
-
-i++;
-}
-
-printf("\n")
-
